2465d27a70b43b39decc4bde2b7f1d04ea014d2b
nudle <33838911+nudle@users.noreply.github.com>
2020-03-21 21:57:53 +0000

    Create streamMirror.bat

---
+++ streamMirror.bat
@@ -0,0 +1,2 @@
+start "" "OculusMirror.exe" --Size 1280 720 --FovTanAngleMultiplier 1.3 1.3 --DisableTimewarp --SymmetricFov --RightEyeOnly --DisableFovStencil --IncludeSystemGui --IncludeNotifications
+exit
